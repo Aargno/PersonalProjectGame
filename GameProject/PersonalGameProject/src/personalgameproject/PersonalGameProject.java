2078ffab4f042848e729ebcf6e2ce0c89f1ce5dc
Aargno <robey.aaron98@gmail.com>
2017-09-23 14:42:15 +0100

    Initial Commit
    Empty classes, no code in game currently.

---
+++ GameProject/PersonalGameProject/src/personalgameproject/PersonalGameProject.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personalgameproject;
+
+/**
+ * Main class, initializes program.
+ * @author robey
+ */
+public class PersonalGameProject {
+
+    /**
+     * Main method, launches state manager.
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
